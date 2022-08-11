759c5c599222d89049e6f579acfdc5e0f7ac7b22
CalebAurand <caurand13@gmail.com>
2022-08-10 21:00:12 -0500

    finished appearances and functionality for homework

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import cars from '../cars.json'
-// Import {useParams} from "react-router-dom" here //
+import {useParams} from 'react-router-dom';
+import { Container, Chip, Paper } from '@mui/material';
 
 // import MUI components here //
 // Container, Paper, Chip //
 
 const Car = (props) => {
+    console.log("cars array", cars);
+    const carId = useParams().id;
+    console.log("carId is", carId, "type is", typeof carId);
+    let car = cars.find(el=>el['id']===parseInt(carId));
+    console.log("car is", car);
+    let keys = Object.keys(car);
+    console.log("keys for car are", keys)
+
     return (
-        <h1>A specific car</h1>
+        <Container sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <Paper elevation={2} sx={{width: 450, display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap', marginTop: 10, padding: 3, paddingTop: 1, backgroundColor: '#1976D2',  borderRadius: 15}} square>
+                <h3 style={{backgroundColor: 'white', width: '100%', textAlign: 'center', padding: 9, borderRadius: 10}}>{car['Name']}</h3>
+                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap'}}>
+                    {keys.map((key, index)=>{
+                        let property = key+": "+car[key];
+                        return(
+                            <Chip key={index} label={property} style={{backgroundColor: 'white', border: 'solid', borderWidth: 1, margin: 1}} variant="filled">
+                            </Chip>
+                        )
+                    })}
+                </div>
+            </Paper>
+        </Container>
     )
 }
 
